696f478bde0b189bc552dd2101043a40eef252ae
gronksearch <michael.shanahan@kellogg.northwestern.edu>
2023-02-22 22:28:35 +0000

    Updated controller for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,14 +8,15 @@ class PostsController < ApplicationController
     def new
         @posts = Post.new
         @places = Place.find_by({ "id" => params["place_id"] })
-        @posts["places_id"] = @places["id"]
-      end
+        @posts["place_id"] = @places["id"]
+    end
     
-      def create
+    def create
         @posts = Post.new
         @posts["title"] = params["post"]["title"]
         @posts["posted_on"] = params["post"]["posted_on"]
         @posts["description"] = params["post"]["description"]
+        @posts["place_id"] = params["place"]["place_id"]
         @posts.save
         redirect_to "/places/#{@posts["places_id"]}"
     end
