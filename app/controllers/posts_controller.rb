3e74d39739577d2847e2f8798a1e2f6c9550ce3e
gronksearch <michael.shanahan@kellogg.northwestern.edu>
2023-02-22 21:40:52 +0000

    Added actions for the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,23 @@
 class PostsController < ApplicationController
-    
-    def index
-        @posts = Post.all
-    end
-    
+
     def show 
-        @name = Place.find_by({ "id" => params["id"] })
-        @posts = Post.where({ "place_id" => @place["id"] })
+        @posts = Post.find_by({ "id" => params["id"] })
+        @places = Places.find_by({ "id" => @places["place_id"] })
+    end
+
+    def new
+        @posts = Post.new
+        @places = Place.find_by({ "id" => params["place_id"] })
+        @posts["places_id"] = @places["id"]
+      end
+    
+      def create
+        @posts = Post.new
+        @posts["title"] = params["post"]["title"]
+        @posts["posted_on"] = params["post"]["posted_on"]
+        @posts["description"] = params["post"]["description"]
+        @posts.save
+        redirect_to "/places/#{@posts["places_id"]}"
     end
 
 end
